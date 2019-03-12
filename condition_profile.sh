c09579f04c2bd35ea59febecc36c11318259d64e
Nick Alexander <nalexander@mozilla.com>
2019-03-12 09:27:27 -0700

    Allow to specify template name.

--- condition_profile.sh
+++ condition_profile.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -x -e -v
 
-: TEMPLATE ${TEMPLATE:=/Users/nalexander/Devel/browsertime/browsersupport/firefox-profile}
+: NAME ${NAME:=firefox-profile}
+: TEMPLATE ${TEMPLATE:=browsersupport/$NAME}
 : PACKAGE ${PACKAGE:=org.mozilla.tv.firefox.gecko.debug}
 : EXTERNAL ${EXTERNAL:=/mnt/sdcard}
 : TMP ${TMP:=/tmp}
-: URL ${URL:=https://google.com}
+: URL ${URL:=https://example.com}
 : SLEEP ${SLEEP:=120}  # In seconds.
 
 for TURBO in true false ; do
@@ -15,13 +16,13 @@ for TURBO in true false ; do
     adb shell pm grant $PACKAGE android.permission.WRITE_EXTERNAL_STORAGE
     adb shell pm grant $PACKAGE android.permission.READ_EXTERNAL_STORAGE
 
-    adb shell rm -rf $EXTERNAL/gecko-profile-turbo-$TURBO
-    adb push $TEMPLATE $EXTERNAL/gecko-profile-turbo-$TURBO
+    adb shell rm -rf $EXTERNAL/$NAME-turbo-$TURBO
+    adb push $TEMPLATE $EXTERNAL/$NAME-turbo-$TURBO
 
     adb shell am start -W -n $PACKAGE/org.mozilla.tv.firefox.MainActivity \
         -a android.intent.action.VIEW -d $URL \
         --ez TURBO_MODE $TURBO \
-        --es args "'-profile $EXTERNAL/gecko-profile-turbo-$TURBO'"
+        --es args "'-profile $EXTERNAL/$NAME-turbo-$TURBO'"
 
     sleep $SLEEP
 
@@ -29,6 +30,6 @@ for TURBO in true false ; do
     # created above.
     adb shell am force-stop $PACKAGE
 
-    rm -rf $TMP/gecko-profile-turbo-$TURBO
-    adb pull $EXTERNAL/gecko-profile-turbo-$TURBO $TMP/gecko-profile-turbo-$TURBO
+    rm -rf $TMP/$NAME-turbo-$TURBO
+    adb pull $EXTERNAL/$NAME-turbo-$TURBO $TMP/$NAME-turbo-$TURBO
 done
