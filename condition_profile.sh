5283c1a222098982f5424a668386947cc824c4bb
Nick Alexander <nalexander@mozilla.com>
2019-04-07 15:37:44 -0700

    Condition profiles for org.mozilla.firefox.

--- condition_profile.sh
+++ condition_profile.sh
@@ -3,33 +3,32 @@ set -x -e -v
 
 : NAME ${NAME:=firefox-profile}
 : TEMPLATE ${TEMPLATE:=browsersupport/$NAME}
-: PACKAGE ${PACKAGE:=org.mozilla.tv.firefox.gecko.debug}
+# : PACKAGE ${PACKAGE:=org.mozilla.tv.firefox.gecko.debug}
+: PACKAGE ${PACKAGE:=org.mozilla.firefox}
 : EXTERNAL ${EXTERNAL:=/mnt/sdcard}
 : TMP ${TMP:=/tmp}
 : URL ${URL:=https://example.com}
 : SLEEP ${SLEEP:=120}  # In seconds.
 
-for TURBO in true false ; do
-    # This will kill the App so that we can safely remove the profile
-    # directories created below.
-    adb shell pm clear $PACKAGE
-    adb shell pm grant $PACKAGE android.permission.WRITE_EXTERNAL_STORAGE
-    adb shell pm grant $PACKAGE android.permission.READ_EXTERNAL_STORAGE
+# This will kill the App so that we can safely remove the profile
+# directories created below.
+adb shell pm clear $PACKAGE
+adb shell pm grant $PACKAGE android.permission.WRITE_EXTERNAL_STORAGE
+adb shell pm grant $PACKAGE android.permission.READ_EXTERNAL_STORAGE
 
-    adb shell rm -rf $EXTERNAL/$NAME-turbo-$TURBO
-    adb push $TEMPLATE $EXTERNAL/$NAME-turbo-$TURBO
+adb shell rm -rf $EXTERNAL/$NAME
+adb push $TEMPLATE $EXTERNAL/$NAME
 
-    adb shell am start -W -n $PACKAGE/org.mozilla.tv.firefox.MainActivity \
-        -a android.intent.action.VIEW -d $URL \
-        --ez TURBO_MODE $TURBO \
-        --es args "'-profile $EXTERNAL/$NAME-turbo-$TURBO'"
+adb shell am start -W -n $PACKAGE/.App \
+    -a android.intent.action.VIEW -d $URL \
+    --ez skipstartpane true \
+    --es args "'-profile $EXTERNAL/$NAME'"
 
-    sleep $SLEEP
+sleep $SLEEP
 
-    # Kill the App so that we can safely pull the profile directory
-    # created above.
-    adb shell am force-stop $PACKAGE
+# Kill the App so that we can safely pull the profile directory
+# created above.
+adb shell am force-stop $PACKAGE
 
-    rm -rf $TMP/$NAME-turbo-$TURBO
-    adb pull $EXTERNAL/$NAME-turbo-$TURBO $TMP/$NAME-turbo-$TURBO
-done
+rm -rf $TMP/$NAME
+adb pull $EXTERNAL/$NAME $TMP/$NAME
